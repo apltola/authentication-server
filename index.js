58290860286f5ccec72838ddf14d6abe5084c9c1
Aleksi Peltola <aleksi@Aleksi-iMac.local>
2018-08-25 08:46:26 +0300

    adding heroku postbuild script

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === 'production') {
   const dist = path.resolve(__dirname, 'client', 'dist');
-  app.use(express.static(dist));
+  app.use('/', express.static(dist));
   
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(dist, 'index.html'));
