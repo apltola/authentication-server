8e45342b8d90d047f7aa3613f05f279b06a6b725
Aleksi Peltola <aleksi@Aleksi-iMac.local>
2018-08-25 08:48:48 +0300

    modifying .gitignore

---
+++ client/dist/static/styles.dacffbdef468427b87c4.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{29:function(n,o,p){n.exports={jotain:"_3P72VVgt9ehG8yeD_mPhAC"}}}]);
+//# sourceMappingURL=styles.dacffbdef468427b87c4.js.map
