fb23060c4cf172c7163f31ea1abb40a0160fe7d2
Aleksi Peltola <aleksi@Aleksi-iMac.local>
2018-08-25 08:32:32 +0300

    adding check for produdction mode

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 5000;
 
-const dist = path.resolve(__dirname, 'client', 'dist');
-app.use(express.static(dist));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(dist, 'index.html'));
-});
 if (process.env.NODE_ENV === 'production') {
+  const dist = path.resolve(__dirname, 'client', 'dist');
+  app.use(express.static(dist));
+  
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(dist, 'index.html'));
+  });
 }
 
 app.listen(PORT, function() { console.log(`RUNNING IN ${PORT}`) });
