37ec941aab5773fccb02431427332519629950fd
Aleksi Peltola <aleksi@Aleksi-iMac.local>
2018-10-06 09:08:03 +0300

    adding stylings to header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import '../styles/sass/components/Header.scss';
 
+
 export default class Header extends Component {
 
   render() {
@@ -11,7 +12,34 @@ export default class Header extends Component {
         <Link to="/">AUTHENTICATION BOILERPLATE</Link>
       </div>
       <div className="auth-container">
-        login or sign up
+        <div className="sign-in-container">
+          <div className="title">
+            sign in
+          </div>
+          <div className="selection">
+            <a href="https://www.npmjs.com/" target="_blank">
+              <button className="google-button">
+                
+                <span className="btn-txt">sign in with Google</span>
+              </button>
+            </a>
+          </div>
+        </div>
+        <div className="separator">
+          /
+        </div>
+        <div className="register-container">
+          <div className="title">
+            register
+          </div>
+          <div className="selection">
+            <a href="https://www.npmjs.com/" target="_blank">
+              <button className="google-button">
+                sign up with Google
+              </button>
+            </a>
+          </div>
+        </div>
       </div>
     </div>
   }
