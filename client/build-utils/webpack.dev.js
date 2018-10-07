254fdc0104d2d01e0ff2a16e8b0884e6a3213c59
Aleksi Peltola <aleksi@Aleksi-iMac.local>
2018-10-07 13:36:34 +0300

    trying facebook oauth stuff...

--- client/build-utils/webpack.dev.js
+++ client/build-utils/webpack.dev.js
@@ -56,6 +56,7 @@ const config = {
     open: true,
     proxy: {
       "/auth/google": { target: 'http://localhost:5000' },
+      "/auth/facebook": { target: 'http://localhost:5000' },
       "/api/*": { target: 'http://localhost:5000' }
     }
   }
